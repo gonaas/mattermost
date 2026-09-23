0070ae862420498293d11947df6b904d42acd247
Gozalo Astudillo <astudillogonzalo99@gmail.com>
2026-09-23 19:54:18 +0200

    feat(branding): replace Mattermost header logo with Wyre chat

--- webapp/channels/src/components/global_header/left_controls/product_menu/product_branding_team_edition/product_branding_free_edition.test.tsx
+++ webapp/channels/src/components/global_header/left_controls/product_menu/product_branding_team_edition/product_branding_free_edition.test.tsx
@@ -9,31 +9,7 @@ import {TestHelper} from 'utils/test_helper';
 import ProductBrandingFreeEdition from './product_branding_free_edition';
 
 describe('ProductBrandingFreeEdition', () => {
-    const baseProps = {};
-
-    test('should show ENTRY EDITION for Entry license', () => {
-        const state = {
-            entities: {
-                general: {
-                    license: TestHelper.getLicenseMock({
-                        IsLicensed: 'true',
-                        SkuShortName: 'entry',
-                    }),
-                },
-            },
-        };
-
-        const {container} = renderWithContext(
-            <ProductBrandingFreeEdition {...baseProps}/>,
-            state,
-        );
-
-        expect(screen.getByText('ENTRY EDITION')).toBeInTheDocument();
-        const logoElement = container.querySelector('svg');
-        expect(logoElement).toBeInTheDocument();
-    });
-
-    test('should show TEAM EDITION for unlicensed', () => {
+    test('should show Wyre chat branding without edition badge', () => {
         const state = {
             entities: {
                 general: {
@@ -46,77 +22,12 @@ describe('ProductBrandingFreeEdition', () => {
         };
 
         const {container} = renderWithContext(
-            <ProductBrandingFreeEdition {...baseProps}/>,
+            <ProductBrandingFreeEdition/>,
             state,
         );
 
-        expect(screen.getByText('TEAM EDITION')).toBeInTheDocument();
-        const logoElement = container.querySelector('svg');
-        expect(logoElement).toBeInTheDocument();
-    });
-
-    test('should show empty badge for Professional license', () => {
-        const state = {
-            entities: {
-                general: {
-                    license: TestHelper.getLicenseMock({
-                        IsLicensed: 'true',
-                        SkuShortName: 'professional',
-                    }),
-                },
-            },
-        };
-
-        renderWithContext(
-            <ProductBrandingFreeEdition {...baseProps}/>,
-            state,
-        );
-
-        // Should not show any edition badge
-        expect(screen.queryByText('ENTRY EDITION')).not.toBeInTheDocument();
+        expect(screen.getByText('Wyre chat')).toBeInTheDocument();
         expect(screen.queryByText('TEAM EDITION')).not.toBeInTheDocument();
-        expect(screen.queryByText('PROFESSIONAL EDITION')).not.toBeInTheDocument();
-    });
-
-    test('should show empty badge for Enterprise license', () => {
-        const state = {
-            entities: {
-                general: {
-                    license: TestHelper.getLicenseMock({
-                        IsLicensed: 'true',
-                        SkuShortName: 'enterprise',
-                    }),
-                },
-            },
-        };
-
-        renderWithContext(
-            <ProductBrandingFreeEdition {...baseProps}/>,
-            state,
-        );
-
-        // Should not show any edition badge
-        expect(screen.queryByText('ENTRY EDITION')).not.toBeInTheDocument();
-        expect(screen.queryByText('TEAM EDITION')).not.toBeInTheDocument();
-        expect(screen.queryByText('ENTERPRISE EDITION')).not.toBeInTheDocument();
-    });
-
-    test('should show empty badge when no license object', () => {
-        const state = {
-            entities: {
-                general: {
-                    license: {},
-                },
-            },
-        };
-
-        renderWithContext(
-            <ProductBrandingFreeEdition {...baseProps}/>,
-            state,
-        );
-
-        // Should not show any edition badge when license object is empty
-        expect(screen.queryByText('ENTRY EDITION')).not.toBeInTheDocument();
-        expect(screen.queryByText('TEAM EDITION')).not.toBeInTheDocument();
+        expect(container.querySelector('svg')).not.toBeInTheDocument();
     });
 });
